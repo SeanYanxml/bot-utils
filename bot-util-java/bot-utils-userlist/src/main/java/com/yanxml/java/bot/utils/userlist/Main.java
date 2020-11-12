f0fe6db6179a6a38fca0a3c283d1523ce881641c
huaianyanxu <huaianyanxu@163.com>
2020-11-12 23:14:25 +0800

    Updatefor userlist util.

---
+++ bot-util-java/bot-utils-userlist/src/main/java/com/yanxml/java/bot/utils/userlist/Main.java
@@ -0,0 +1,5 @@
+package com.yanxml.java.bot.utils.userlist;
+
+public class Main {
+
+}
---
+++ bot-util-java/bot-utils-userlist/src/main/java/com/yanxml/java/bot/utils/userlist/utils/UserListHTML.java
@@ -0,0 +1,20 @@
+package com.yanxml.java.bot.utils.userlist.utils;
+
+/**
+ * 用来读取数据文件。拼接后输出为HTML文件。
+ * 
+ * */
+public class UserListHTML {
+	
+	// 主要流程方法
+	public static void mainFunction() {
+		//1. 读取文件内的人物列表信息
+		//2. 拼接HTML页面字符串
+		//3. 将HTML页面字符串创建成HTML文件输出
+	}
+	
+	public static void main() {
+		
+	}
+
+}
